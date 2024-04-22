bac58c07323f583edccd35cf7a4ba5917e82e4c3
Richard Bramley <rabramley@gmail.com>
2024-04-22 14:22:24 +0100

    Base templates

--- wifi_lma/settings/base.py
+++ wifi_lma/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "wagtailmenus",
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
